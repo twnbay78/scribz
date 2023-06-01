1daae84d388d9729f22087b135b87b785b3bde0f
twnbay78 <leoscarano6@gmail.com>
2023-06-01 04:14:37 -0400

    Adding error handling and log statements

--- config_nvim_astrovim.sh
+++ config_nvim_astrovim.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-set -u
+set -ue
 
 echo "cleaning nvim from system"
-sudo rm -r ~/.config/nvim
-sudo rm -r ~/.local/state/nvim
-sudo rm -r ~/.local/share/nvim
-sudo rm ~/nvim.appimage
+sudo rm -r ~/.config/nvim || echo "~/.config/nvim probably doesn't exist, therefore not deleting"
+sudo rm -r ~/.local/state/nvim || echo "~/.local/state/nvim probably doesn't exist, therefore not deleting"
+sudo rm -r ~/.local/share/nvim || echo "~/.local/share/nvim probably doesn't exist, therefore not deleting"
+sudo rm ~/nvim.appimage || echo ""
 
 echo "reinstalling nvim and astrovim"
 cd ~/
@@ -22,8 +22,10 @@ sudo update-alternatives --install /usr/bin/view view "${CUSTOM_NVIM_PATH}" 110
 sudo update-alternatives --install /usr/bin/vim vim "${CUSTOM_NVIM_PATH}" 110
 sudo update-alternatives --install /usr/bin/vimdiff vimdiff "${CUSTOM_NVIM_PATH}" 110
 
+echo "Initializing astrovim"
 git clone https://github.com/AstroNvim/AstroNvim ~/.config/nvim
 
+echo "installing patched font"
 git clone git@github.com:ryanoasis/nerd-fonts.git ~/nerd-fonts/ --depth 1
 ./install ./install.sh ShareTechMono
 
