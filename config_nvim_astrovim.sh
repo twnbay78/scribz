fc36e608042cc386c0434cad35e60e5fdddf485a
twnbay78 <leoscarano6@gmail.com>
2023-06-01 04:21:01 -0400

    Checking in .bashrc

--- config_nvim_astrovim.sh
+++ config_nvim_astrovim.sh
@@ -7,10 +7,12 @@ sudo rm -r ~/.local/state/nvim || echo "~/.local/state/nvim probably doesn't exi
 sudo rm -r ~/.local/share/nvim || echo "~/.local/share/nvim probably doesn't exist, therefore not deleting"
 sudo rm ~/nvim.appimage || echo ""
 
-echo "reinstalling nvim and astrovim"
+echo "Installing nvim"
 cd ~/
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
 chmod u+x nvim.appimage
+
+echo "Lazy loading astrovim and lazy"
 git clone --depth 1 https://github.com/AstroNvim/AstroNvim ~/.config/nvim
 
 # Set defaults 
@@ -22,8 +24,9 @@ sudo update-alternatives --install /usr/bin/view view "${CUSTOM_NVIM_PATH}" 110
 sudo update-alternatives --install /usr/bin/vim vim "${CUSTOM_NVIM_PATH}" 110
 sudo update-alternatives --install /usr/bin/vimdiff vimdiff "${CUSTOM_NVIM_PATH}" 110
 
-echo "Initializing astrovim"
-git clone https://github.com/AstroNvim/AstroNvim ~/.config/nvim
+echo "initializing git, password must be entered"
+ssh-start || echo "failed to initialize ssh agent, alias probably isn't set in ~/.bashrc"
+git clone git@github.com:twnbay78/astrovim_config.git ~/.config/nvim/lua/user
 
 echo "installing patched font"
 git clone git@github.com:ryanoasis/nerd-fonts.git ~/nerd-fonts/ --depth 1
